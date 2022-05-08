a43e1dac7a878534a6618633657ecc2acd634c82
Tran Minh Luyen <tranminhluyen.1993@gmail.com>
2022-05-09 04:20:16 +0700

    Add files via upload

---
+++ psds/config.js
@@ -0,0 +1,10 @@
+module.exports = [
+  { id: 0, path: 'psds/mine/luyen22.psd', scale: 'center' },
+  { id: 1, path: 'psds/mine/luyen22gray.psd', scale: 'center' },
+  { id: 2, path: 'psds/mine/luyen22gray.psd', scale: 'center' },
+  { id: 3, path: 'psds/mine/rug1.psd', scale: 'fit' },
+  { id: 4, path: 'psds/mine/rug2.psd', scale: 'fit' },
+  { id: 5, path: 'psds/mine/blanket1.psd', scale: 'fit' },
+  { id: 6, path: 'psds/mine/blanket2.psd', scale: 'fit' },
+  { id: 7, path: 'psds/mine/quilt.psd', scale: 'fit' },
+]
