3eb163fac0f10b2854e29d9df140a7a813b30d19
Tran Minh Luyen <tranminhluyen.1993@gmail.com>
2022-07-21 01:24:05 +0700

    Update config.js

--- psds/config.js
+++ psds/config.js
@@ -1,10 +1,4 @@
 module.exports = [
-  { id: 0, path: 'psds/mine/luyen22.psd', scale: 'center' },
-  { id: 1, path: 'psds/mine/luyen22gray.psd', scale: 'center' },
-  { id: 2, path: 'psds/mine/luyen22gray.psd', scale: 'center' },
-  { id: 3, path: 'psds/mine/rug1.psd', scale: 'fit' },
-  { id: 4, path: 'psds/mine/rug2.psd', scale: 'fit' },
-  { id: 5, path: 'psds/mine/blanket1.psd', scale: 'fit' },
-  { id: 6, path: 'psds/mine/blanket2.psd', scale: 'fit' },
-  { id: 7, path: 'psds/mine/quilt.psd', scale: 'fit' },
+  { id: 0, path: 'psds/mine/tshirt_men.psd', scale: 'fit' },
+  { id: 1, path: 'psds/thuan/tumbler1.psd', scale: 'fit' }
 ]
